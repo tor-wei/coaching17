f07875e5d761647abfca04ed801abfcc08b35298
tor-wei <gohtorwei@gmail.com>
2025-07-21 15:49:12 +0800

    Added egress rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,3 +68,11 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tcp" {
   ip_protocol       = "tcp"
   to_port           = 8080
 }
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_egress" {
+  security_group_id = aws_security_group.ecs_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  ip_protocol       = "-1"
+  to_port           = 0
+}
